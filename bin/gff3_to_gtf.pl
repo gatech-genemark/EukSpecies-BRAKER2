81163635a5b435bc2e5d3bc4659355ab5865e9fb
Alexandre Lomsadze <sandro.lomsadze@gmail.com>
2019-06-19 19:28:18 -0400

    cleaning

---
+++ bin/gff3_to_gtf.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/perl
+#---------------------------------
+# Alex Lomsadze
+# GaTech 2019
+#
+# move GFF3 to GTF with stops included
+# --------------------------------
+
+use strict;
+use warnings;
+
+my $gff3 = shift;
+my $gtf = shift;
+my $v = 1;
+
+if ( !$gff3 ) { die "error, input file name is missing"; }
+if ( !$gtf ) { die "error, output file name is missing"; }
+
+my %trans_to_gene = ();
+
+LoadIds($gff3, \%trans_to_gene );
+
+open( my $IN, $gff3) or die "error on open file: $gff3\n";
+open( my $OUT, ">", $gtf) or die "error on open file: $gtf\n";
+while(<$IN>)
+{
+	if ( /\tCDS\t/ )
+	{
+		my $trans = '';
+
+		chomp;
+
+		if ( /Parent=([^;]+)/ )
+		{
+			$trans = $1;
+		}
+
+		if ( !$trans )
+			{ die "error, unexpected line format found: $_"; }
+
+		my @arr = split( ',' , $trans );
+
+		foreach my $label (@arr)
+		{
+			if ( ! exists $trans_to_gene{$label} )
+				{ die "error, Parent is missing for: $label\n";}
+
+			my $line = $_;
+			$line =~ s/\tParent=.*$/\t/;
+
+			print $OUT $line;
+
+			my $attr = "gene_id \"$trans_to_gene{$label}\"\;";
+			$attr .= " transcript_id \"$label\"\;\n";
+
+			print $OUT $attr;
+		}
+	}
+}
+close $OUT;
+close $IN;
+
+# -------------------
+sub LoadIds
+{
+	my $name = shift;
+	my $ref = shift;
+
+	open( my $IN, $name) or die "error on open file: $name\n";
+	while(<$IN>)
+	{
+		if ( /^#/ ) {next;}
+
+		if ( /\tmRNA\t/ )
+		{
+#			if ($v) { print $_; }
+
+			my $trans = '';
+			my $gene = '';
+
+			if ( /ID=([^;]+)/ )
+			{
+				$trans = $1;
+			}
+
+			if ( /Parent=([^;]+)/ )
+			{
+				$gene = $1;
+			}
+
+			if( !$trans or !$gene )
+				{ die "error, unexpect format found: $_"; }
+
+			$ref->{$trans} = $gene;
+
+#			if ($v) { print "$trans $gene\n"; }
+		}
+	}
+	close $IN;
+}
+# -------------------
+
+
---
+++ bin/gff_to_gff_subset.pl
@@ -0,0 +1,218 @@
+#!/usr/bin/perl
+# --------------------------------------------
+# Alex Lomsadze
+# GaTech
+# January 2019
+# --------------------------------------------
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+# --------------------------------------------
+
+if ( $#ARGV == -1 ) { print PrintUsage(); exit 1; }
+
+my $in = '';
+my $out = '';
+my $list = '';
+my $v = '';
+my $col = 1;
+my $swap = 0;
+
+my $result = GetOptions
+(
+  'in=s'   => \$in,
+  'out=s'  => \$out,
+  'list=s' => \$list,
+  'verbose'=> \$v,
+  'col=i'  => \$col,
+  'swap'   => \$swap,
+);
+
+if ( !$result ) { print "error on cmd"; print PrintUsage(); exit 1; }
+if ( !$in or !$out ) { die "error on cmd: out or in is missing\n"; }
+if (( $in eq $out ) or ( $list eq $out)) { die "error on cmd: out equals in or list\n"; }
+if ( !$list ) { die "error on cmd: list is missing \n"; }
+
+# --------------------------------------------
+
+my %h;
+LoadList( $list, \%h );
+
+# --------------------------------------------
+
+my $count_in = 0;
+my $count_out = 0;
+
+my %allowed =
+(
+	"gene" => 1,
+	"mRNA" => 1,
+	"CDS" => 1,
+	"exon" => 1,
+
+#	"Repeat" => 1,
+#	"similarity" => 1,
+#	"match" => 1,
+
+#	"five_prime_UTR" => 1,
+#	"three_prime_UTR" => 1,
+	"intron" => 1,
+#	"transposable_element" => 1,
+
+#	"ncRNA" => 1,
+#	"snoRNA" => 1,
+#	"pseudogene" => 1,
+#	"snRNA" => 1,
+#	"tRNA" => 1,
+#	"rRNA" => 1,
+#	"pre_miRNA" => 1,
+#	"miRNA" => 1,
+);
+
+my %found;
+
+open( my $IN, $in ) or die "Can't open $in: $!\n";
+open( OUT, ">", $out ) or die "Can't open $out: $!\n";
+
+while ( my $record = <$IN> )
+{
+	next if ( $record =~ /^\s*$/ );
+
+	++$count_in;
+
+	if ( $record =~ /^##gff-version 3/ )
+	{
+		print OUT $record;
+		next;
+	}
+
+	if (( $record =~/^##sequence-region\s+(\S+)\s*/ ) or ( $record =~/^(\S+)\t(\S+)\t(\S+)\t/ ))
+	{
+		my $id = $1;
+
+		if ( defined $2 and defined $3 and $2 )
+		{
+			next if ( ! exists $allowed{$3} );
+		}
+
+		if ( exists( $h{$id} ) )
+		{
+			$record =~ s/^$id/$h{$id}/;
+
+			print OUT $record;
+			++$count_out;
+			$found{$id} +=1 ;
+		}
+	}
+}
+
+close $IN;
+close OUT;
+
+if ($list)
+{
+	CheckAllFound( \%found );
+}
+
+print "$count_in in $in file\n";
+print "$count_out in $out file\n";
+
+# --------------------------------------------
+sub CheckAllFound
+{
+	my $ref = shift;
+
+	for my $key (keys %$ref )
+	{
+		if ( ! exists $ref->{$key} )
+		{
+			print "warning, record was not found: $ref->{$key}\n";
+		}
+	}	
+}
+# --------------------------------------------
+sub LoadList
+{
+	my $name = shift;
+	my $ref = shift;
+	
+	if (!$name )
+	{
+		print "error, file name is empty\n";
+		exit 1;
+	} 
+	
+	open( my $F, $name ) or die "error on  open $name: $!\n";
+	while( my $line = <$F> )
+	{
+		next if ( $line =~ /^\s*$/ );
+		next if ( $line =~ /^#/ );
+		
+		if ( $line =~ /^\s*(\S+)\s+(\S+)\s*$/ )
+		{
+			my $col_a = $1;
+			my $col_b = $2;
+
+			if ( $col == 2 )
+			{
+				$col_a = $2;
+				$col_b = $1;
+			}
+
+
+			if ( exists $ref->{$col_a} )
+			{
+				print "error, duplicated entry found in the list: $name\n";
+				exit 1;
+			}
+
+			if ( $swap )
+			{
+				$ref->{$col_a} = $col_b;
+			}
+			else
+			{
+				$ref->{$col_a} = $col_a;
+			}
+		}
+		else
+		{
+			print "warning, unexpected format was found in $name: $line";
+		}
+	}
+	close $F;
+
+	if ( scalar keys %$ref < 1 )
+	{
+		print "error, list is empty: $name\n";
+		exit 1;
+	}
+
+	if ( $v )
+	{
+		for my $key (keys %$ref)
+		{
+			print $key ." ". $ref->{$key} ."\n";
+		}
+	}
+}
+# --------------------------------------------
+sub PrintUsage
+{
+	my $txt = "Usage: $0  --in <file name>  --out <file name>  --list <file name>
+
+  This program takes as input GFF formatted anotation and
+  outputs GFF records only for sequence ID's listed on the --list file.
+
+  List file may have two columns
+  --list <file name> : name_1 name_2
+  This scpecifies which column to use as
+  --col  <column>    :   1  or  2
+  --swap             : swap one name to another
+";
+	return $txt;
+}
+# --------------------------------------------
+
