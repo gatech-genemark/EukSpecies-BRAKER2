50f7dbc87cd2c05ba7d392b888b4ee0f1a1617cb
Alexandre Lomsadze <sandro.lomsadze@gmail.com>
2019-08-07 23:13:44 -0400

    with pseudo potion now

--- bin/compare_intervals_exact.pl
+++ bin/compare_intervals_exact.pl
@@ -20,6 +20,8 @@ use warnings;
 use Getopt::Long qw( GetOptions );
 use Data::Dumper;
 
+my $VERSION = "v1_2019";
+
 # ------------------------------------------------
 my $v = '';
 my $debug = 0;
@@ -27,6 +29,8 @@ my $debug = 0;
 my $f1 = '';
 my $f2 = '';
 my $f3 = '';
+
+my $pseudo = '';
 
 my $compare_cds = 0;
 my $compare_introns = 0;
@@ -75,6 +79,25 @@ ParseGFF( $f1, \%h1 );
 ParseGFF( $f2, \%h2 );
 ParseGFF( $f3, \%h3 ) if $f3;
 
+if ($pseudo)
+{
+	my %pseudo_h = LoadPseudo( $pseudo );
+
+	my %intervals = ();
+
+	%intervals = HashKeysToArrIntervals( \%h1 );
+	RemoveOverlappingWithPseudo( \%pseudo_h, \%intervals, 0 );
+
+	%intervals = HashKeysToArrIntervals( \%h2 );
+	RemoveOverlappingWithPseudo( \%pseudo_h, \%intervals, 1, \%h2 );
+
+	if ( $f3 )
+	{
+		%intervals = HashKeysToArrIntervals( \%h3 );
+		RemoveOverlappingWithPseudo( \%pseudo_h, \%intervals, 1, \%h3 );
+	}
+}
+
 if( !$f3 )
 {
 	Compare2();
@@ -87,6 +110,128 @@ else
 exit 0;
 
 # ================= subs =========================
+sub RemoveOverlappingWithPseudo
+{
+	my $aa = shift;
+	my $bb = shift;
+	my $in_bb = shift;
+	my $ref = shift;
+
+	my @arr = ();
+	my %hash = ();
+
+	foreach my $key ( keys %{$aa} )
+	{
+		next if ( ! exists $bb->{$key} );
+
+		my $size_aa = scalar @{$aa->{$key}};
+		my $size_bb = scalar @{$bb->{$key}};
+
+		my $i = 0;
+		my $j = 0;
+		
+		print "# resolving overlaps for $key $size_aa $size_bb\n" if $debug;
+
+		while( $i < $size_aa && $j < $size_bb )
+		{
+			# i.R < j.L
+			if ( $aa->{$key}[$i][1] < $bb->{$key}[$j][1] )
+			{
+				$i += 1;
+				next;
+			}
+
+			# i.L > j.R
+			if ( $aa->{$key}[$i][0] > $bb->{$key}[$j][1] )
+			{
+				$j += 1;
+				next;
+			}
+
+			# i.L <= j.R  and i.R >= j.L
+			if (( $aa->{$key}[$i][0] <= $bb->{$key}[$j][1] ) && ( $aa->{$key}[$i][1] >= $bb->{$key}[$j][0] ))
+			{
+				if ( $in_bb )
+				{
+					push @arr, $bb->{$key}[$j][2];
+				}
+				else
+				{
+					push @arr, [ @{$aa->{$key}[$i]} ];
+
+					$hash{ $key ."_". $aa->{$key}[$i][0] ."_". $aa->{$key}[$i][1] } += 1;
+				}
+
+				$j += 1;
+				next;
+			}
+
+			die "error in logic\n";
+		}
+	}
+
+	if ( $in_bb )
+	{
+		foreach my $key (@arr)
+		{
+			delete $ref->{$key};
+		}
+	}
+	else
+	{
+		foreach my $key ( keys %{$aa} )
+		{
+			my @new_arr = ();
+
+			foreach my $value ( @{$aa->{$key}} )
+			{
+				my $test_key = $key ."_". $value->[0] ."_". $value->[1];
+
+				if ( ! exists $hash{$test_key} )
+				{
+					push @new_arr, [ $value->[0], $value->[1] ];
+				}
+			}
+
+			@{$aa->{$key}} = @new_arr;
+		}
+	}
+
+	if ($v)
+	{
+		my $size = scalar @arr;
+		print "# total overlaps removed: $size\n";
+#		print Dumper(\@arr);
+	}
+}
+# ------------------------------------------------
+sub HashKeysToArrIntervals
+{
+	my $ref = shift;
+
+	my %h = ();
+
+	foreach my $key (keys %{$ref})
+	{
+		if ( $key =~ /^(.+)_(\d+)_(\d+)_[+-.]$/ or $key =~ /^(.+)_(\d+)_(\d+)_[+-.]_[012.]$/ )
+		{
+			push @{$h{$1}}, [$2, $3, $key];
+		}
+		else
+			{ die "error, unexpected key format found: $key\n"; }
+	}
+
+	foreach my $key (keys %h)
+	{
+		my @arr_a = @{$h{$key}};
+		my @arr_b = sort{ $a->[0] <=> $b->[0] } (@arr_a);
+
+		$h{$key} = \@arr_b;
+	}
+
+	return %h;
+}
+# ------------------------------------------------
 sub Compare3
 {
 	my $c1 = scalar keys %h1;
@@ -163,9 +308,18 @@ sub Compare3
 	die "error, no intervals in file: $f2\n" if (!$c2);
 	die "error, no intervals in file: $f3\n" if (!$c3);
 
+	if ($v)
+	{
+		print "\n";
+		print "#in\tmatch\tunique\t\%match\tCDS\n"  if $compare_cds;
+		print "#in\tmatch\tunique\t\%\tIntron\n"    if $compare_introns;
+		print "#in\tmatch\tunique\t\%\tStarts\n"    if $compare_starts;
+		print "#in\tmatch\tunique\t\%\tStops\n"     if $compare_stops;
+		print "#in\tmatch\tunique\t\%\tDonors\n"    if $compare_donors;
+		print "#in\tmatch\tunique\t\%\tAcceptors\n" if $compare_acceptors;
+	}
+
 	print "\n";
-	print "#in\tin_all\tin_1\tin_2\tin_3\tunique\t\%match_all\tCDS\n" if ($v and !$compare_introns );
-	print "#in\tin_all\tin_1\tin_2\tin_3\tunique\t\%match_all\tIntron\n" if ($v and $compare_introns );
 	print $c1 ."\t". $match ."\t". "0"    ."\t". $c1_c2 ."\t". $c1_c3 ."\t". $c1_uniq ."\t". sprintf( "%.2f", 100.0*$match/$c1 ) ."\t". $f1 ."\n";
 	print $c2 ."\t". $match ."\t". $c1_c2 ."\t". "0"    ."\t". $c2_c3 ."\t". $c2_uniq ."\t". sprintf( "%.2f", 100.0*$match/$c2 ) ."\t". $f2 ."\n";
 	print $c3 ."\t". $match ."\t". $c1_c3 ."\t". $c2_c3 ."\t". "0"    ."\t". $c3_uniq ."\t". sprintf( "%.2f", 100.0*$match/$c3 ) ."\t". $f3 ."\n";
@@ -281,13 +435,17 @@ sub Compare2
 	die "error, no intervals in file: $f1\n" if (!$total_c1);
 	die "error, no intervals in file: $f2\n" if (!$total_c2);
 
-	print "\n";
-	print "#in\tmatch\tunique\t\%match\tCDS\n" if ($v and $compare_cds );
-	print "#in\tmatch\tunique\t\%\tIntron\n" if ($v and $compare_introns );
-	print "#in\tmatch\tunique\t\%\tStarts\n" if ($v and $compare_starts );
-	print "#in\tmatch\tunique\t\%\tStops\n" if ($v and $compare_stops );
-	print "#in\tmatch\tunique\t\%\tDonors\n" if ($v and $compare_donors );
-	print "#in\tmatch\tunique\t\%\tAcceptors\n" if ($v and $compare_acceptors );
+	if ($v)
+	{
+		print "\n";
+		print "#in\tmatch\tunique\t\%match\tCDS\n"  if $compare_cds;
+		print "#in\tmatch\tunique\t\%\tIntron\n"    if $compare_introns;
+		print "#in\tmatch\tunique\t\%\tStarts\n"    if $compare_starts;
+		print "#in\tmatch\tunique\t\%\tStops\n"     if $compare_stops;
+		print "#in\tmatch\tunique\t\%\tDonors\n"    if $compare_donors;
+		print "#in\tmatch\tunique\t\%\tAcceptors\n" if $compare_acceptors;
+	}
+
 	print "\n";
 	print  $total_c1 ."\t". $match ."\t". $c1_uniq ."\t". sprintf( "%.2f", 100.0*$match/$total_c1 ) ."\t". $f1 ."\n";
 	print  $total_c2 ."\t". $match ."\t". $c2_uniq ."\t". sprintf( "%.2f", 100.0*$match/$total_c2 ) ."\t". $f2 ."\n";
@@ -325,6 +483,95 @@ sub TestCount
 	$size += scalar (keys %{$ref4}) if ($ref4);
 
 	die "error in counting: $count $size\n" if ( $size != $count );
+}
+# ------------------------------------------------
+sub LoadPseudo
+{
+	my ($name, $ref) = @_;
+
+	my %h = ();
+
+	my $in_regions = 0;
+
+	open( my $IN, $name ) or die "error on open file $name: $!\n";
+	while( my $line = <$IN> )
+	{
+		next if( $line =~ /^\s*#/ );
+		next if( $line =~ /^\s*$/ );
+
+		next if ( $line !~ /\t/ );
+		
+		if( $line =~ /^(\S+)\t\S+\t(\S+)\t(\d+)\t(\d+)\t\S+\t([-+.])\t(\S+)\s*/ )
+		{
+			my $id     = $1;
+			my $type   = $2;
+			my $start  = $3;
+			my $end    = $4;
+			my $strand = $5;
+			my $ph     = $6;
+
+			push @{$h{$id}}, [$start, $end];
+			$in_regions += 1;
+		}
+	}
+	close $IN;
+
+	my $out_regions = 0;
+
+	foreach my $key (keys %h)
+	{
+		my @arr_a = @{$h{$key}};
+		my @arr_b = sort{ $a->[0] <=> $b->[0] } (@arr_a);
+		my @arr_c = ();
+
+		my $size = scalar @arr_b;
+
+		for( my $i = 0; $i < $size; )
+		{
+			my $L = $arr_b[$i][0];
+			my $R = $arr_b[$i][1];
+
+			my $j = 0;
+
+			for( $j = $i+1; $j < $size; $j += 1 )
+			{
+				if ( $R >= $arr_b[$j][0] )
+				{
+					if ( $R < $arr_b[$j][1] )
+					{
+						$R = $arr_b[$j][1];
+						print "# merged $i $j $L $R $arr_b[$j][0] $arr_b[$j][1]\n" if $debug
+					}
+					else
+					{
+						print "# full overlap found $i $j $L $R $arr_b[$j][0] $arr_b[$j][1]\n" if $debug
+					} 
+				}
+				else
+				{
+					last;
+				}
+			}
+
+			$i = $j;
+
+			push @arr_c, [$L, $R];
+		}
+
+		$h{$key} = \@arr_c;
+
+		$out_regions += (scalar @arr_c);
+	}
+
+	if ($v)
+	{
+		print "# pseudo regions in: $in_regions\n";
+		print "# regions after merging: $out_regions\n";
+	}
+
+#	print Dumper(\%h);
+
+	return %h
 }
 # ------------------------------------------------
 sub ParseGFF
@@ -442,13 +689,13 @@ sub ParseGFF
 		print "# Acceptors in file $name: ". (scalar keys %$ref) ."\n" if $compare_acceptors;
 	}
 	
-	print Dumper($ref) if $debug;
+#	print Dumper($ref) if $debug;
 }
 # ------------------------------------------------
 sub CheckBeforeRun
 {
-	die "error, file is missing $0: option --f1\n" if( ! -e $f1 );
-	die "error, file is missing $0: option --f2\n" if( ! -e $f2 );
+	die "error, file not found: option --f1 $f1\n" if( ! -e $f1 );
+	die "error, file not found: option --f2 $f2\n" if( ! -e $f2 );
 
 	if ( $f3 )
 	{
@@ -461,7 +708,12 @@ sub CheckBeforeRun
 		die "error, output file name matches input file\n" if ( $f3 and ( $out_file eq $f3 ));
 	}
 
-	if ($original)
+	if ( $pseudo )
+	{
+		die "error, file not found: option --pseudo $pseudo\n" if ( ! -e $pseudo );
+	}
+
+	if ( $original )
 	{
 		if ( $original == 1 or $original == 2 )
 			{;}
@@ -497,6 +749,7 @@ sub ParseCMD
 		'unique2'   => \$unique2,
 		'unique3'   => \$unique3,
 		'original=i' => \$original,
+		'pseudo=s'  => \$pseudo,
 	);
 
 	die "error on command line\n" if( !$opt_results );
@@ -530,11 +783,16 @@ $0  --f1 [name]  --f2 [name]
 
 Compare intervals from input files:
  
-   --f1  [name]  file in GFF or GTF format
-   --f2  [name]  file in GFF or GTF format
+   --f1  [name]  file in GFF style format
+   --f2  [name]  file in GFF style format
 
 Optional:
-   --f3  [name]  file in GFF or GTF format
+   --f3  [name]  file in GFF style format
+
+   --pseudo [name] file with pseudogene coordinates in GFF style format;
+                   with this option program
+                   excludes pseudo regions overlaping with --f1
+                   excludes CDS region from --f2 and --f3 which overlap pseudo regions
 
 Default comparision is done for 'CDS' type
 
@@ -566,6 +824,9 @@ Output subset of records into file based on the overlap status:
 General:
    --verbose
    --bedug
+
+Version: $VERSION
+
 );
 	exit 1;
 }
